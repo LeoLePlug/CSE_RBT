b7a243e9b1445ac0e14ae4abe45ae8bb5d462391
LeoLePlug <139858319+LeoLePlug@users.noreply.github.com>
2023-09-07 14:16:23 +0200

    Create cse-rbt.php

---
+++ cse-rbt.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: CSE-RBT
+ * Plugin URI: https://leoleplug.com/
+ * Description: Ce plugin permet de faire fonctionner les fonctionnalités de remboursement sur facture du site (module de recherche, envoie, mail, etc...). #made by leoleplug agency, votre agence web de confiance.
+ * Version: 0.1
+ * Author: Leoleplug Agency
+ * Author URI: https://leoleplug.com/
+ */
